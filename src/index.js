ff39f8e5312f910d6a63408a3ae2b3ae094276d5
fearm3 <ibraaa1628@gmail.com>
2022-03-02 00:59:19 +0300

    add fix

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { ProductsProvider } from "./context/products_context";
-import { FilterProvider } from "./context/filter_context";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </ProductsProvider>,
+  <App />,
+
   document.getElementById("root")
 );
 
